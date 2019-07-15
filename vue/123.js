a129d549d53682cc59a574601578a6eb6e57f263
LQ1996 <674174093@qq.com>
2019-07-15 18:39:28 +0800

    7-15-1

---
+++ vue/123.js
@@ -0,0 +1,12 @@
+
+var a = "fsfsdaffsdfdasfsdafad"
+var b = a.length
+for(i=0; i<b; i++){
+    console.log(i)
+}
+
+
+var a = "fsfsdaffsdfdasfsdafad"
+for(i=0; i<a.length; i++){
+    console.log(i)
+}
